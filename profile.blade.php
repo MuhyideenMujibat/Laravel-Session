909ce4c12f5bd66356b00aa56f635b45f3ebaabd
Muhyideen Mujibat <96301638+MuhyideenMujibat@users.noreply.github.com>
2023-11-15 03:44:55 +0100

    Create profile.blade.php

---
+++ profile.blade.php
@@ -0,0 +1,4 @@
+<h1> Profile Page</h1>
+<h2> Hello, {{session('fullname')}} </h2>
+<h2> Your password is below </h2> <h2 style="color:pink;">"{{session('password')}}" </h2>
+<a href="/logout"> Logout </a> 
